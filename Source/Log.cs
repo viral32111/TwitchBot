ce15a230793dfbd77c8bd7fc2542cd9dc844277e
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-09-01 22:55:00 +0100

    Add error & debug log levels

--- Source/Log.cs
+++ Source/Log.cs
@@ -6,12 +6,20 @@ namespace TwitchBot {
 			Console.WriteLine( "[{0:dd-MM-yyyy HH:mm:ss.fff zzz}] [{1}] {2}", DateTime.UtcNow, severity.ToUpper(), string.Format( format, objects ) );
 		}
 
+		public static void Debug( string format, params object?[] objects ) {
+			Write( "DEBUG", format, objects );
+		}
+
 		public static void Info( string format, params object?[] objects ) {
 			Write( "INFO", format, objects );
 		}
 
 		public static void Warn( string format, params object?[] objects ) {
 			Write( "WARN", format, objects );
+		}
+
+		public static void Error( string format, params object?[] objects ) {
+			Write( "ERROR", format, objects );
 		}
 	}
 }
