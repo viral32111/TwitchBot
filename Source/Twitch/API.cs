82c53e87003503b796cee63dfcba445b5452400b
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-11-07 17:53:59 +0000

    Add support for request body to API

--- Source/Twitch/API.cs
+++ Source/Twitch/API.cs
@@ -2,26 +2,35 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
 namespace TwitchBot.Twitch {
 	public static class API {
-		public async static Task<JsonObject> Request( string endpoint, HttpMethod? method = null, Dictionary<string, string>? queryParameters = null ) {
+		public async static Task<JsonObject> Request( string endpoint, HttpMethod? method = null, Dictionary<string, string>? queryParameters = null, JsonObject? payload = null ) {
 
 			// Construct the URL
-			Uri targetUrl = new( $"https://{Config.TwitchAPIBaseURL}/{endpoint}?{queryParameters?.ToQueryString()}" );
+			string queryString = queryParameters != null ? $"?{queryParameters.ToQueryString()}" : "";
+			Uri targetUrl = new( $"https://{Config.TwitchAPIBaseURL}/{endpoint}{queryString}" );
 
 			// Create the request, defaulting to GET
 			HttpRequestMessage httpRequest = new( method ?? HttpMethod.Get, targetUrl.ToString() );
+
+			// Always expect a JSON response
+			httpRequest.Headers.Accept.Add( MediaTypeWithQualityHeaderValue.Parse( "application/json" ) );
 
 			// Add the OAuth credentials as headers
 			httpRequest.Headers.Add( "Client-Id", Config.TwitchOAuthIdentifier );
 			httpRequest.Headers.Authorization = Shared.UserAccessToken!.GetAuthorizationHeader();
 
+			// Set the request body, if one is provided
+			if ( payload != null ) httpRequest.Content = new StringContent( payload.ToJsonString(), Encoding.UTF8, "application/json" );
+
 			// Send the request & make sure to retry when it fails due to token expiry
 			HttpResponseMessage httpResponse = await Shared.httpClient.SendAsync( httpRequest );
-			Log.Debug( "API request: {0} '{1}' => {2} {3}", httpRequest.Method.ToString(), httpRequest.RequestUri!.ToString(), ( int ) httpResponse.StatusCode, httpResponse.StatusCode.ToString() );
+			Log.Debug( "API request: {0} '{1}' '{2}' => {3} {4}", httpRequest.Method.ToString(), httpRequest.RequestUri?.ToString(), payload?.ToJsonString(), ( int ) httpResponse.StatusCode, httpResponse.StatusCode.ToString() );
 			if ( httpResponse.StatusCode == HttpStatusCode.Unauthorized ) {
 				Log.Warn( "User access token has expired, refreshing & saving..." );
 				await Shared.UserAccessToken.DoRefresh();
